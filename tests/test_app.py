595dfe3b1380973516b9c7cf224b84ff962b1976
nicolasgghg <nicolasgghg12@gmail.com>
2026-03-30 09:14:57 -0300

    test: simulando erro

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,5 +9,5 @@ def test_home():
 def test_status():
     client = app.test_client()
     response = client.get("/status")
-    assert response.status_code == 200
+    assert response.status_code == 201
     assert response.json["status"] == "ok"
